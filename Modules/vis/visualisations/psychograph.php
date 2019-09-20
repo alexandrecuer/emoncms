6ee73cb0883271a54a2e80360cc1383e95e1985d
Alexandre CUER <alexandre.cuer@cerema.fr>
2019-09-20 15:36:23 +0200

    Update psychograph.php

--- Modules/vis/visualisations/psychograph.php
+++ Modules/vis/visualisations/psychograph.php
@@ -19,15 +19,15 @@
 <script>
 //console.log(path);
 var flot0='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.min.js"><\/script>';
-var flot1='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.selection.min.js"></script>';
-var flot2='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.touch.min.js"></script>';
-var flot3='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.togglelegend.min.js"></script>';
-var flot4='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.time.min.js"></script>';
-var flot5='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.stack.min.js"></script>';
-var flot6='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.canvas.js"></script>';
-var flot7='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/plugin/saveAsImage/lib/base64.js"></script>';
-var flot8='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/plugin/saveAsImage/lib/canvas2image.js"></script>';
-var flot9='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/plugin/saveAsImage/jquery.flot.saveAsImage.js"></script>';
+var flot1='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.selection.min.js"><\/script>';
+var flot2='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.touch.min.js"><\/script>';
+var flot3='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.togglelegend.min.js"><\/script>';
+var flot4='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.time.min.js"><\/script>';
+var flot5='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.stack.min.js"><\/script>';
+var flot6='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/jquery.flot.canvas.js"><\/script>';
+var flot7='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/plugin/saveAsImage/lib/base64.js"><\/script>';
+var flot8='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/plugin/saveAsImage/lib/canvas2image.js"><\/script>';
+var flot9='<script language="javascript" type="text/javascript" src="'+path+'Lib/flot/plugin/saveAsImage/jquery.flot.saveAsImage.js"><\/script>';
 document.write(flot0);
 document.write(flot1);
 document.write(flot2);
